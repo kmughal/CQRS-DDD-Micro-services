f6ecb777d78b4cb46be6aa61827efc3647ece5c1
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-01-12 18:20:36 +0000

    Add a simple ioc for console project
    - A simple way to having decorator pattern

---
+++ console-app-ioc/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace IoC.Decorator.Example
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = IocProvider.ServiceProvider.GetService<IFoo>();
+            foo.PrintHelloWorld();
+        }
+    }
+}
